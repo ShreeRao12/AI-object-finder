9f67c7015e19343f621b08c9befddc344a1f5a1a
ShreeRao12 <89769918+ShreeRao12@users.noreply.github.com>
2022-07-02 16:52:11 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,26 @@
+status = "";
+function preload()
+{
+ 
+}
+function setup()
+{
+ canvas = createCanvas(480,380);
+ canvas.center();
+ video = createCapture(VIDEO);
+ video.hide();
+}
+function draw()
+{
+    image(video, 0, 0, 480, 380);
+}
+function start()
+{
+    objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("status").innerHTML = "Status: Detecting objects...";
+function modelLoaded()
+{
+    console.log("Model Loaded!");
+    status = true;
+}
+}
